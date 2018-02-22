a96714360ff6e0d6b349ddf7a0d742735f119fb4
Therése Barmer <therese.barmer@gmail.com>
2018-02-22 08:56:03 +0100

    Push messages from text input into array

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,8 @@
 let message = document.getElementById("message");
 let button = document.getElementById("submit");
+
+let messages = [];
+
+button.addEventListener('click', function() {
+    messages.push({ message: message.value });
+});
