18c2e97a19c879e4edee896b6821abaa3cea46db
Therése Barmer <therese.barmer@gmail.com>
2018-02-22 10:52:41 +0100

    Get messages from firebase
    Display these in the page.

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,9 @@ let db = firebase.database();
 let message = document.getElementById("message");
 let button = document.getElementById("submit");
 let output = document.getElementById("messageOutput");
+let header = document.createElement("h2");
+header.innerText = "Messages";
+let child = document.createElement("p");
 
 let currentDate = new Date();
 
@@ -28,6 +31,17 @@ button.addEventListener('click', function() {
         message: message.value,
         timeStamp: currentDate.toString()
     });
+    let messages = db.ref('/messages');
     output.innerHTML = "";
+    messages.on('value', function(snapshot) {
+        for(let message of Object.values(snapshot.val())) {
+            child.innerHTML += `
+            <div>${message.message}</div>
+            <div>${message.timeStamp}</div>
+        `;
+        }
+        output.appendChild(header);
+        output.appendChild(child);
+    });
     message.value = "";
 });
