fb28c483838b72d1c92e34a227b6ac338c03dfa2
Therése Barmer <therese.barmer@gmail.com>
2018-02-22 09:03:29 +0100

    Display message below textbox

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 let message = document.getElementById("message");
 let button = document.getElementById("submit");
+let output = document.getElementById("messageOutput");
 
 let messages = [];
 
 button.addEventListener('click', function() {
     messages.push({ message: message.value });
+    output.innerText = message.value;
+    message.value = "";
 });
